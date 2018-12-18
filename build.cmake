23c4d13dd4d312f3048d78a31bebde9df5471090
Gevorg Adamyan <gevorg.adamyan@here.com>
2018-12-18 18:25:18 +0100

    Configured cmake project with the dependencies to be able to build lib, test and example projects.

--- build.cmake
+++ build.cmake
@@ -1,5 +1,9 @@
 #!/usr/bin/env cmake -P
 
-include (${CMAKE_CURRENT_LIST_DIR}/common.cmake)
+function(build)
+  set(BUILD_DIR "build")
+  file(MAKE_DIRECTORY ${BUILD_DIR})
+  execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_LIST_DIR} WORKING_DIRECTORY ${BUILD_DIR})
+endfunction()
 
-build_xcode ()
+build()
